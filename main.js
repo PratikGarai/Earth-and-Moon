87b545cc03be0087da4d0790be931cad427f0fe5
PratikGarai <pratikgarai0208@gmail.com>
2021-06-25 04:28:56 +0530

    First shape

--- main.js
+++ main.js
@@ -16,4 +16,23 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
-renderer.render(scene, camera);
+
+
+const geometry = new THREE.TorusGeometry(10, 3, 15, 15);
+const material = new THREE.MeshBasicMaterial({
+    color : "red",
+    wireframe : true
+});
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+
+function animate() {
+    requestAnimationFrame(animate);
+    torus.rotation.x += 0.01;
+    torus.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+
+animate();
